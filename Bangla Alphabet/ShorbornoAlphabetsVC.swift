dccfcf26bca8bfaa498b90b9119d7f7b99d5ae0c
AfzaL Hossain <afzalhossain@AfzaLs-iMac.local>
2017-06-19 23:03:36 +0600

    first commit

---
+++ Bangla Alphabet/ShorbornoAlphabetsVC.swift
@@ -0,0 +1,20 @@
+//
+//  ShorbornoAlphabetVC.swift
+//  Bangla Alphabet
+//
+//  Created by Md AfzaL Hossain on 3/26/17.
+//  Copyright © 2017 Md AfzaL Hossain. All rights reserved.
+//
+
+import UIKit
+
+class ShorbornoAlphabetVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+
+}
